c8162e85a3ced80853472c15f37597ec65cbab2b
kerbalcadet <kerbalcadet@gmail.com>
2020-05-16 17:01:26 -0500

    removed redundant cfg reference

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -15,6 +15,6 @@ GM.Name =  "Gun Game"
 function GM:Initialize()
     if Spawns[1] then SpawnFile(Spawns)
     else Spawns = SpawnFile() end
-    WeapsAdd(CFG)
+    WeapsAdd()
     ended = false
 end
