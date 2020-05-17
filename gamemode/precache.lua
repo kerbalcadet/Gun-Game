ef85cf0e6b082b6486d8f8d007c460ed63fac6d0
John Woodruff <rocketpanda40@gmail.com>
2020-05-17 01:16:17 -0500

    Fix string rename with DOI weapons

--- gamemode/precache.lua
+++ gamemode/precache.lua
@@ -1,6 +1,7 @@
 function PreCacheVMs()
     for k, v in pairs(WeapFile())
     do
+        util.PrecacheModel(string.gsub(WeapFile()[k][1], "cw_kk_ins2_doi", "v"))
         util.PrecacheModel(string.gsub(WeapFile()[k][1], "cw_kk_ins2", "v"))
     end
 end
