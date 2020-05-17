609e69acbdc5c8c770bda4270fd37e3c5d2bb09d
kerbalcadet <kerbalcadet@gmail.com>
2020-05-17 13:35:14 -0500

    while loop -> for loop (crashes)

--- gamemode/weapons.lua
+++ gamemode/weapons.lua
@@ -2,14 +2,19 @@ weaps = {}
 
 if SERVER then
     
-function GetRandomWeap(weap_type)
+function GetRandomWeap(wtype)
+    if !wtype then return end
+
     local t = WeapFile()
+    local weapt = {}
 
-    repeat
-        tmp_weap =t[math.random(#t)]
-    until tmp_weap[2] ==weap_type and tmp_weap[2] !="disabled"
+    for k, weap in ipairs(t) do
+        if weap[2] == wtype then table.insert(weapt, weap) end
+    end
 
-    return tmp_weap
+    if !weapt[1] then return {} end
+
+    return weapt[math.random(#weapt)]
 end
 
 function WeapsAdd()
@@ -29,21 +34,18 @@ function WeapsAdd()
     "explosive",
     "launcher"
     }
-    local weap_num =WEAP_NUM:GetInt()
-    local grp_num_per =math.ceil(weap_num/10)
-    local grp_num =(weap_num-(weap_num%10))/grp_num_per
+    
+    local w_num = WEAP_NUM:GetInt()
 
-    for weap_type =1, 10, 1
-    do
-        if(weap_type <=grp_num) then
-            for weap =1, grp_num_per, 1
-            do
-                cur_weap =GetRandomWeap(types[weap_type])
-                table.insert(weaps, {class =cur_weap[1], type =cur_weap[2]})
-            end
-        else
-            cur_weap =GetRandomWeap(types[weap_type])
-            table.insert(weaps, {class =cur_weap[1], type =cur_weap[2]})
+    for k, wtype in pairs(types) do
+        for i = 1, math.ceil(w_num/#types) do
+            if #weaps >= w_num then return end
+
+            local weap = GetRandomWeap(wtype)
+
+            if weap[1] then 
+                print(table.ToString(weap))
+                table.insert(weaps, {class = weap[1], type = weap[2]}) end
         end
     end
 end
